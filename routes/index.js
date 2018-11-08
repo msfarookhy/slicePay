4bba68122d4b4ff1d709ae36dd2ba26c1c2d400d
msfarookhy <msfarookhy@gmail.com>
2018-11-08 22:22:27 +0530

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+///product rout
+router.get('/product', (req, res) => {
+  res.render('product');
+});
 
+// login rout
+router.get('/login', (req, res) => {
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup')
+
+});
 module.exports = router;
