04a0b93c711f3e7f59b740426b51a2c2a190aaa4
msfarookhy <msfarookhy@gmail.com>
2018-11-13 22:57:15 +0530

    index route some changes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 ///product rout
+router.get('/home', (req, res) => {
+  res.render('product');
+});
+
 router.get('/product', (req, res) => {
   res.render('product');
 });
