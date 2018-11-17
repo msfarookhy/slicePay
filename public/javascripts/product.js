38eed8fe55f135325700a1bf41e1738adbc8f674
msfarookhy <msfarookhy@gmail.com>
2018-11-17 15:43:00 +0530

    templates of product added

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -1,52 +1,48 @@
-profile = [];
-profile.database = {};
+product = [];
+product.database = {};
 const loadAssets = function () {
-    $.getJSON("/myprofile/", function (data) {
-        profile.database = data;
-        profile.init();
+    $.getJSON("/api/product/", function (data) {
+        product.database = data;
+        product.init();
     });
 };
 
-profile.init = function () {
-    // profile.del();
-    // profile.update();
-    // profile.addR();
-    profile.generateMarkup();
+product.init = function () {
+    product.generateMarkup();
 };
 
-profile.generateMarkup = function () {
+product.generateMarkup = function () {
     var template = "";
 
-    $.each(profile.database, function (index) {
-        db = profile.database;
+    $.each(product.database, function (index) {
+        db = product.database;
         id = db[index];
         //   console.log(id);
 
         template += '<div data-id="' + id._id + '">';
 
         template += '<div class="card" style="width: 25rem;">';
-        template += '<img class="card-img-top" src="' + id.image + '"></img>';
+        template += '<img class="card-img-top" src="' + id.Image + '"></img>';
         template += '<div class="card-body">';
-        template += '<h5 class="fas fa-map-marker-alt">' + id.name + "</h5>";
-        template += '<h3 class="card-title">' + id.emailId + "</h3>";
-        template += '<p  class="card-text">' + id.address + "</p><br>";
-        template += '<p  class="card-text">' + id.number + "</p><br>";
+        template += '<h5 class="fas fa-map-marker-alt">' + id.Name + "</h5>";
+        template += '<h3 class="card-title">' + id.quantity + "</h3>";
+        template += '<p  class="card-text">' + id.price + "</p><br>";
         template += '<div id="hide">';
-        template += "<div>";
-        template += '<button  class="del btn btn-primary">Delete</button>';
-        template += "</div>";
-        template +=
-            '<button class="update btn btn-primary  ">Edit Detials</button>';
-        template += "</div>";
+        // template += "<div>";
+        // template += '<button  class="del btn btn-primary">Delete</button>';
+        // template += "</div>";
+        // template +=
+        //     '<button class="update btn btn-primary  ">Edit Detials</button>';
+        // template += "</div>";
 
         template += "</div>";
         template += "</div>";
         template += "</div>";
     });
     $(".content").append(template);
-    // profile.del();
-    // profile.update();
-    // profile.addR();
+    // product.del();
+    // product.update();
+    // product.addR();
 };
 // on click
 
